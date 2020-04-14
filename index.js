11e6a8fabdcf3f9b73b39106237220bae8c3c34d
Alexander Makarof <makarofalex@gmail.com>
2020-04-14 21:01:02 +0300

    edit cfg, but with error

--- index.js
+++
@@ -1,1 +0,0 @@
-// mongodb+srv://alex123:<password>@cluster0-melmx.mongodb.net/test?retryWrites=true&w=majority
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("API alive"));
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API alive");
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`ping pong ${PORT}`));
